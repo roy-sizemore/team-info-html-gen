9a194bb28b52c24a03b38b3f403deb52aae39253
Roy Sizemore <rsizemore@swampcastle.home>
2021-06-02 15:46:20 -0400

    Fixed bug in index.js

--- index.js
+++ index.js
@@ -4,49 +4,37 @@ const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 
-const generateOrgChart = (data) =>
-    `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-        <title></title>
-    </head>
-    <body>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-        <script src="../src/index.js"></script>
-    </body>
-    </html>`;
+const startTeam = () => {
 
-const generateManager = () => {
-inquirer
-    .prompt([
-        {
-            name: 'name',
-            message: "Please enter the manager's name: ",
-            type: 'input'
-        },
-        {
-            name: 'email',
-            message: "Pleae enter the manager's email: ",
-            type: 'input'
-        },
-        {
-            name: 'id',
-            message: "Please enter the manager's ID: ",
-            type: 'input'
-        },
-        {
-            name: 'officeNumber',
-            message: "Please enter the manger's office number: ",
-            type: 'input'
-        }
-    ])
-    .then((answers) => {
-        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-        generateEmployee();
-    });
+    const generateManager = () => {
+        inquirer
+        .prompt([
+            {
+                name: 'name',
+                message: "Please enter the manager's name: ",
+                type: 'input'
+            },
+            {
+                name: 'email',
+                message: "Pleae enter the manager's email: ",
+                type: 'input'
+            },
+            {
+                name: 'id',
+                message: "Please enter the manager's ID: ",
+                type: 'input'
+            },
+            {
+                name: 'officeNumber',
+                message: "Please enter the manger's office number: ",
+                type: 'input'
+            }
+        ])
+        .then((answers) => {
+            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+            generateEmployee();
+        });
+    };
 
     const generateEmployee = () => {
         inquirer
@@ -131,5 +119,7 @@ inquirer
             generateEmployee();
         })
     }
+
+    generateManager();
 };
 
